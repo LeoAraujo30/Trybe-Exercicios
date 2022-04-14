a1a06342668a5f2080521ea82d24cc2f65ffe8f0
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-04-14 00:56:48 -0300

    exercicios dia 5.1

---
+++ modulo 1/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/Dia 5.1 - JavaScript - DOM e seletores/script.js
@@ -0,0 +1,17 @@
+document.getElementById('header-container').style.backgroundColor = 'yellowgreen'
+
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'lightpink'
+
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'yellow'
+
+let titulos1 = document.querySelectorAll('.emergency-tasks h3')
+
+titulos1[0].style.backgroundColor = 'purple'
+titulos1[1].style.backgroundColor = 'purple'
+
+let titulos2 = document.querySelectorAll('.no-emergency-tasks h3')
+
+titulos2[0].style.backgroundColor = 'black'
+titulos2[1].style.backgroundColor = 'black'
+
+document.getElementById('footer-container').style.backgroundColor = 'darkgreen'
